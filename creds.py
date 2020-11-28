59fb10521318a81306926dcd0e6dfceb662aab7b
kuyagic <kuyagic@users.noreply.github.com>
2020-11-28 12:14:38 +0800

    Update creds.py

--- creds.py
+++ creds.py
@@ -6,3 +6,4 @@ class my():
     BOT_TOKEN = os.getenv("BOT_TOKEN")   # from @botfather
     API_ID = int(os.getenv("API_ID"))    # from https://my.telegram.org/apps
     API_HASH = os.getenv("API_HASH")     # from https://my.telegram.org/apps
+    PREFIX = os.getenv("PREFIX")
